40a040faecab11d3e1e8ac7aed1bf484f257b154
Florian Spychiger <spychiger88@gmail.com>
2018-11-27 16:42:08 +0100

    created import script

---
+++ db/ImportData.sql
@@ -0,0 +1,15 @@
+.mode csv
+
+.import ../data/Eclipse/assigned_to.csv assigned_to
+.import ../data/Eclipse/bug_status.csv bug_status
+.import ../data/Eclipse/cc.csv cc
+.import ../data/Eclipse/component.csv component
+.import ../data/Eclipse/op_sys.csv op_sys	
+.import ../data/Eclipse/priority.csv priority	
+.import ../data/Eclipse/product.csv product
+.import ../data/Eclipse/reports.csv reports
+.import ../data/Eclipse/resolution.csv resolution
+.import ../data/Eclipse/severity.csv severity	
+.import ../data/Eclipse/version.csv version
+
+
